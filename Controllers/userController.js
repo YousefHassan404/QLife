c624fe0bab0fe664c0c925343403c1962f7c8a38
YousefHassan404 <aaa01095836766@gmail.com>
2024-12-03 03:00:21 +0200

    5ve version of the QLife Backend

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -63,3 +63,17 @@ export const getAllUsers = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+
+
+export const getUserByEmail = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.params.email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- Routers/userRoutes.js
+++ Routers/userRoutes.js
@@ -5,15 +5,18 @@ import {
   updateUser,
   deleteUser,
   getAllUsers,
+  getUserByEmail
 } from "../Controllers/userController.js";
 
 const router = express.Router();
 
 // Routes
+router.get("/email/:email", getUserByEmail);
 router.post("/", createUser); // Create
 router.get("/:id", getUserById); // Read by ID
 router.put("/:id", updateUser); // Update
 router.delete("/:id", deleteUser); // Delete
 router.get("/", getAllUsers); // Get all
+
 
 export default router;
